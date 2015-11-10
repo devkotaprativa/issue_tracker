c84ff008ea1962117773f4b46f0cd2f3abd8bee6
prativa <devkotaprativa@gmail.com>
2015-11-10 10:21:52 +0545

    new controller projects was generated with actions index show new create destroy and update.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+
+  get 'projects/show'
+
+  get 'projects/new'
+
+  get 'projects/create'
+
+  get 'projects/destroy'
+
+  get 'projects/update'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
