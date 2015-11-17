3e7fe15c135dc1d593bea1303408292ef1a34b50
prativa <devkotaprativa@gmail.com>
2015-11-17 15:16:32 +0545

    new action 'complete' defined for issues controller which sets the status of the issue to complete. Route for the above action was defined in the routes file, and link to the action was added in the projects/show template.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :projects do
     resources :issues do
       post 'issues/add_members'
+      post 'issues/complete'
     end
     post 'issues/show_members'
   end
